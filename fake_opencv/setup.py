de3148a943d0f58410d1163d8279dde56da557c2
Abhay Bhopi <bhopiabhay9@gmail.com>
2026-09-08 23:57:00 +0530

    Add dummy opencv-contrib package to bypass libGL error and remove packages.txt

---
+++ fake_opencv/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="opencv-contrib-python",
+    version="99.9.9",
+    description="A dummy package to trick mediapipe into not installing the GUI version of OpenCV.",
+)
